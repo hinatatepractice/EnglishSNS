c07bdc7b86caa0a2db5ab9819e5d3d745afc393b
hinatatepractice <hinata.tezuka.foreign@gmail.com>
2022-07-09 18:34:16 +0900

    ユーザーのプロフィール画面実装

--- app/Models/Follower.php
+++ app/Models/Follower.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Follower extends Model
 {
@@ -19,5 +20,15 @@ class Follower extends Model
     ];
 
     public $timestamps = false;     
-    public $incrementing = false;   
+    public $incrementing = false;
+    
+    public function getFollowCount($user_id)
+    {
+        return $this->where('following_id', $user_id)->count();
+    }
+
+    public function getFollowerCount($user_id)
+    {
+        return $this->where('followed_id', $user_id)->count();
+    }
 }
